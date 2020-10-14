d89f94e646ae6d2efe1a450d30e9122d96645144
Benito Zenz <s_zenz19@stud.hwr-berlin.de>
2020-10-14 17:20:25 +0200

    #36 delete duplicate login route

--- BackEnd/src/routes/api/auth.js
+++ BackEnd/src/routes/api/auth.js
@@ -25,27 +25,6 @@ createRoutes([
         }
     },
     {
-        path: '/login', 
-        method: 'post', 
-        strategy: 'local',
-        callback: (req, res, user) => 
-        {
-            req.logIn(user, { session: false }, err => 
-                {
-                    if (err) return res.status(400).json({ errors: err });
-
-                    const userId = user.id;
-                    const rolle = user.id === 'userOne' ? 'teacher' : 'student';
-
-                    const body = { _id: userId };
-                    const request_token = jwt.sign({ user: body }, 'TOP_SECRET');
-
-                    return res.status(200).json({ userId, rolle, request_token });
-                }
-            );
-        }
-    },
-    {
         path: '/woAmI', 
         method: 'get', 
         strategy: 'jwt',
