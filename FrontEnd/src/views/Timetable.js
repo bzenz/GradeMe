f861fd40f685263e13e76950d3786f3ecc825de1
Robin <robin0507@gmx.de>
2020-10-14 14:20:15 +0200

    Schülerhauptmenü mit Stundenplanmock und Kommunikationsdashboardsmock

---
+++ FrontEnd/src/views/Timetable.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Stundenplan from "../images/Stundenplan_Mock.jpg"
+
+export default function Timetable() {
+    return (
+        <div>
+            <img src={Stundenplan} alt="this is car image" />
+        </div>
+    )
+}
