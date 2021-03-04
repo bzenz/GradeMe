a5a792f72c492bd21a2cb225b826e76852c7f67d
Robin <robin0507@gmx.de>
2021-03-04 15:11:47 +0100

    Änderungen in subject administration

--- FrontEnd/src/components/admin/SubjectAdministration.js
+++ FrontEnd/src/components/admin/SubjectAdministration.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react";
-import { StyleSheet, View, Text } from 'react-native';
+import {StyleSheet, View, Text } from 'react-native';
 import {connect} from "react-redux";
 import {generalNativeStyles} from "../../styles/GeneralStyles";
-import {Button, Input, ListItem, Card } from "react-native-elements";
-import {subjectMockArray} from "../../../utils/mocks";
+import {Button, Input, ListItem, Card, CheckBox } from "react-native-elements";
+import {SERVER} from "../../../index";
+import {setErrorData} from "../../actions/errorActions";
 
 const customStyles = StyleSheet.create({
     subjectCard: {
@@ -16,63 +17,121 @@ const customStyles = StyleSheet.create({
     },
     button1: {
        padding: "40px",
-    }
+    },
+    checkBoxContainerStyle: {
+        backgroundColor: "#ffffff",
+        borderWidth: 0,
+    },
 })
 
-function subjectAdministration() {
+function subjectAdministration(props) {
+    const [,setState] = useState();  //Wird für ein Forceupdate genutzt
     const [showNewSubjectInputField, setShowNewSubjectInputField] = useState(false);
     const [subjectList, setSubjectList] = useState([]);
-    const [newSubjectNameField, setNewSubjectNameField] = useState("");
     const [newSubjectInputFieldErrorMessage, setNewSubjectInputFieldErrorMessage] = useState("");
     const [editedSubjectId, setEditedSubjectId] = useState(null);
-    const [subjectEditTextfield, setSubjectEditTextfield] = useState("");
+    const [checkBoxIsEnabled, setCheckBoxIsEnabled] = useState(false);
 
     useEffect(() => {
-        setSubjectList(subjectMockArray.reverse());
+        const requestBody = JSON.stringify({request_token: props.request_token})
+        fetch(SERVER + "/api/subject/getAll",
+            {
+                "method": "POST",
+                "headers": {'Content-Type': 'application/json'},
+                "body": requestBody
+            }).then(response => response.json())
+            .then(data => setSubjectList(data.reverse()))
     }, [])
-
 
     const handleCreateSubjectClick = () => {
         setShowNewSubjectInputField(true);
     }
 
+    let subjectEditTextfield = "";
     const handleEditSubjectClick = (_editedSubjectId, editedSubjectName) => {
         setEditedSubjectId(_editedSubjectId);
-        setSubjectEditTextfield(editedSubjectName);
+        subjectEditTextfield = editedSubjectName;
     }
 
+    //Neues Fach erstellen
+    let newSubjectNameField = "";
+    const handleSaveSubject = () => {
+        if(newSubjectNameField === "") {
+            setNewSubjectInputFieldErrorMessage("Der Name des Fachs darf nicht leer sein");
+            return;
+        }
+
+        const setNewSubjectIdAndSetNewSubjectList = (newSubjectId) => {
+            const newSubjectList = subjectList;
+            newSubjectList.unshift({subjectId: newSubjectId.subjectId, subjectName: newSubjectNameField, deactivated: false});
+            setSubjectList(newSubjectList);
+            setNewSubjectInputFieldErrorMessage("");
+            setShowNewSubjectInputField(false);
+            newSubjectNameField = "";
+        }
+        const requestBody = JSON.stringify({
+            request_token: props.request_token,
+            name: newSubjectNameField
+        })
+        fetch(SERVER + "/api/subject/create",
+            {
+                "method": "POST",
+                "headers": {'Content-Type': 'application/json'},
+                "body": requestBody,
+            }).then(response => response.json())
+            .then((data) => (setNewSubjectIdAndSetNewSubjectList(data)))
+    }
+
+    //Fach editieren
     const handleSaveEditedSubjectName = () => {
         for (let i = 0; i< subjectList.length; i++){
-            if(subjectList[i].Id === editedSubjectId){
-                subjectList[i].name = subjectEditTextfield;
+            if(subjectList[i].subjectId === editedSubjectId){
+                subjectList[i].subjectName = subjectEditTextfield;
+                const requestBody = JSON.stringify({subjectId: editedSubjectId, newName: subjectEditTextfield,request_token: props.request_token})
+                fetch(SERVER + "/api/subject/edit",
+                    {
+                        "method": "POST",
+                        "headers": {'Content-Type': 'application/json'},
+                        "body": requestBody
+                    })
                 setEditedSubjectId(null);
                 break;
             }
         }
     }
 
-    const handleDeactivateSubject = () => {
-        alert("Diese Funktion ist noch nicht verfügbar");
+    //Fach aktivieren/deaktivieren
+    const changeSubjectStatus = (subjectId, deactivate) => {
+
+        const requestBody = JSON.stringify({subjectId: subjectId ,request_token: props.request_token})
+        fetch(SERVER + "/api/subject/deactivate",
+            {
+                "method": "POST",
+                "headers": {'Content-Type': 'application/json'},
+                "body": requestBody
+            });
+
+        for (let i = 0; i < subjectList.length; i++){
+            if(subjectList[i].subjectId === subjectId){
+                const newSubjectList = subjectList;
+                console.log("deactivated value: " + newSubjectList[i].deactivated)
+                newSubjectList[i].deactivated = deactivate;
+                console.log("deactivated value: " + newSubjectList[i].deactivated)
+                setSubjectList(newSubjectList);
+                setState({}); //ForceUpdate. Aus irgendeinem Grund reicht setSubjectList nicht, um das deaktivierte
+                // subject direkt aus der Liste zu streichen...deshalb jetzt die (nach 1.5h nicht mehr so quick) and dirty Methode
+                break;
+            }
+        }
+
+        if(deactivate){
+            alert("Fach wurde deaktiviert");
+        } else {
+            alert("Fach wurde Aktiviert");
+        }
     }
 
-    const handleSaveSubject = () => {
-        if(newSubjectNameField === "") {
-            setNewSubjectInputFieldErrorMessage("Der Name des Fachs darf nicht leer sein");
-            return;
-        }
-        let highestId = 0;
-        subjectList.forEach((element) => {
-            if(element.Id > highestId){
-                highestId = element.Id;
-        }
-        })
-        const newSubjectList = subjectList;
-        newSubjectList.unshift({Id: ++highestId, name: newSubjectNameField});
-        setSubjectList(newSubjectList);
-        setNewSubjectNameField("");
-        setNewSubjectInputFieldErrorMessage("");
-        setShowNewSubjectInputField(false);
-    }
+
 
     return (
         <View style={generalNativeStyles.root}>
@@ -84,13 +143,18 @@ function subjectAdministration() {
                 containerStyle={{width: "70%"}}
                 onPress={handleCreateSubjectClick}
                 title={"Fach hinzufügen"}
-            >
-            </Button>
+            />
+            <CheckBox
+                title={"Deaktivierte Fächer anzeigen"}
+                checked={checkBoxIsEnabled}
+                containerStyle={customStyles.checkBoxContainerStyle}
+                onPress={() => (setCheckBoxIsEnabled(!checkBoxIsEnabled))}
+            />
             {showNewSubjectInputField?
                 <Card containerStyle={customStyles.subjectCard}>
                     <Input
                         style={[generalNativeStyles.halfWidth, generalNativeStyles.marginTop]}
-                        onChangeText={textinput => setNewSubjectNameField(textinput)}
+                        onChangeText={textinput => newSubjectNameField = textinput}
                         errorMessage={newSubjectInputFieldErrorMessage}
                     />
                     <Button title={"Fach speichern"} buttonStyle={generalNativeStyles.button1} onPress={() => handleSaveSubject()}/>
@@ -99,54 +163,67 @@ function subjectAdministration() {
 
 
             {subjectList.map((listItem) => {
-                return (
-                    <Card containerStyle={customStyles.subjectCard}>
-                        <ListItem >
-                            <ListItem.Content style={customStyles.listItemContent}>
-                                <ListItem.Content style={{ maxWidth: "25%"}}>
-                                    <Text style={{fontSize: 22}}>
-                                        {"FachId: " + listItem.Id}
-                                    </Text>
-                                </ListItem.Content>
-                                {editedSubjectId === listItem.Id?
-                                    <ListItem.Content>
-                                        <Input
-                                            defaultValue={listItem.name}
-                                            label={"Neuer Fachname"}
-                                            onChangeText={textinput => setSubjectEditTextfield(textinput)}
-                                        />
-                                    </ListItem.Content>:
-                                    <ListItem.Content >
+                console.log("neurender")
+                if(checkBoxIsEnabled || !listItem.deactivated) {
+                    console.log(listItem.subjectName + " wird gerendert")
+                    return (
+                        <Card containerStyle={customStyles.subjectCard}>
+                            <ListItem>
+                                <ListItem.Content style={customStyles.listItemContent}>
+                                    <ListItem.Content style={{maxWidth: "25%"}}>
                                         <Text style={{fontSize: 22}}>
-                                            {listItem.name}
+                                            {"FachId: " + listItem.subjectId}
                                         </Text>
                                     </ListItem.Content>
-                                }
-                                {editedSubjectId === listItem.Id?
-                                    <Button
-                                        title={"Speichern"}
-                                        buttonStyle={[generalNativeStyles.button1, generalNativeStyles.marginRight,{ backgroundColor: "#00f459"}]}
-                                        onPress={() => handleSaveEditedSubjectName()}
-                                    />:
-                                    <Button
-                                        title={"Bearbeiten"}
-                                        buttonStyle={[generalNativeStyles.button1, generalNativeStyles.marginRight]}
-                                        onPress={() => handleEditSubjectClick(listItem.Id, listItem.name)}
-                                    />
-                                }
-                                <Button
-                                    title={"Deaktivieren"}
-                                    buttonStyle={generalNativeStyles.button1}
-                                    onPress={() => handleDeactivateSubject()}
-                                />
-                            </ListItem.Content>
+                                    {editedSubjectId === listItem.subjectId ?
+                                        <ListItem.Content>
+                                            <Input
+                                                defaultValue={listItem.subjectName}
+                                                label={"Neuer Fachname"}
+                                                onChangeText={textinput => subjectEditTextfield = textinput}
+                                            />
+                                        </ListItem.Content> :
+                                        <ListItem.Content>
+                                            <Text style={{fontSize: 22}}>
+                                                {listItem.subjectName}
+                                            </Text>
+                                        </ListItem.Content>
+                                    }
+                                    {editedSubjectId === listItem.subjectId ?
+                                        <Button
+                                            title={"Speichern"}
+                                            buttonStyle={[generalNativeStyles.button1, generalNativeStyles.marginRight, {backgroundColor: "#00f459"}]}
+                                            onPress={() => handleSaveEditedSubjectName()}
+                                        /> :
+                                        <Button
+                                            title={"Bearbeiten"}
+                                            buttonStyle={[generalNativeStyles.button1, generalNativeStyles.marginRight]}
+                                            onPress={() => handleEditSubjectClick(listItem.subjectId, listItem.subjectName)}
+                                        />
+                                    }
+                                    {listItem.deactivated?
+                                        <Button
+                                            title={"Aktivieren"}
+                                            buttonStyle={generalNativeStyles.button1}
+                                            onPress={() => changeSubjectStatus(listItem.subjectId, false)}
+                                       />:
+                                        <Button
+                                            title={"Deaktivieren"}
+                                            buttonStyle={generalNativeStyles.button1}
+                                            onPress={() => changeSubjectStatus(listItem.subjectId, true)}
+                                        />
+                                    }
 
-                        </ListItem>
-                    </Card>
+                                </ListItem.Content>
+
+                            </ListItem>
+                        </Card>
                     )
+                }
             })}
         </View>
+
     )
 }
 
-export default connect()(subjectAdministration)
+export default connect((state) => ({request_token: state.loginReducer.request_token}), {setErrorData})(subjectAdministration)
